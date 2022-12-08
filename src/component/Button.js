bded768dce0086d088d67917002fafab3dd41a61
natia <nati.shonia37@gmail.com>
2022-12-09 01:08:15 +0400

    Create Button.js

---
+++ src/component/Button.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import classes from './button.module.css'
+
+function Button({value, handler}) {
+  return (
+    <div className={value === "C" || value === "=" ? `${classes.calcBtns} ${classes.res}` : `${classes.calcBtns}`}
+    onClick={()=> handler(value)}
+    >
+       {value}
+    </div>
+  )
+}
+
+export default Button
