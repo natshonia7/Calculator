6e50fc5d88edd5901563f3f5173c8d0efccf5d82
natia <nati.shonia37@gmail.com>
2022-12-09 01:08:27 +0400

    Create Demo.js

---
+++ src/component/Demo.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import classes from './demo.module.css'
+
+function Demo() {
+  return (
+    <div className={classes.inner}>
+      demo
+    </div>
+  )
+}
+
+export default Demo
